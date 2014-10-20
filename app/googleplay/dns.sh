c42f3be0143d9112b1ba370190a5f623f43a1b7c
Jianqing Peng <pengjianqing@gmail.com>
2014-10-20 23:56:56 +0800

    adjust the column domain and ip

--- app/googleplay/dns.sh
+++ app/googleplay/dns.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 HOSTS="${HOME}/etc/hosts.d/hosts"
-curl -k "https://vps.pjq.me:8443/FTServlet/openapi/getip?domain=play.google.com" -o ${HOSTS}
+curl -k "https://vps.pjq.me:8443/FTServlet/openapi/getip?domain=play.google.com" -o hosts.tmp
+awk -F " " '{print $2"     "$1}' hosts.tmp  > ${HOSTS}
+rm hosts.tmp
